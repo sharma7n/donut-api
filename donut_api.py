086adfdf205e411ed7543043ee62af611d492b19
sharma7n <sharma7n@gmail.com>
2016-07-28 22:35:52 -0500

    heroku db

--- donut_api.py
+++ donut_api.py
@@ -9,6 +9,7 @@ from flask_potion import Api, ModelResource
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ("DATABASE_URL")
 db = SQLAlchemy(app)
 
 class Donut(db.Model):
